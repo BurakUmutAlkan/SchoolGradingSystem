0f78b5507827188392f6724884e64afb34a8445e
BurakUmutAlkan <burumalkan96@gmail.com>
2023-02-06 22:35:01 +0300

    NOTE: There is a issue with the database when it is being created, so it cannot be created. Issue states that it comes from a connection-fault or faulty-instance. Will look into it.

--- SchoolGradingSystem/Data/SchoolDbContext.cs
+++ SchoolGradingSystem/Data/SchoolDbContext.cs
@@ -19,5 +19,7 @@ namespace SchoolGradingSystem.Data
         public DbSet<Class> Classes => Set<Class>();
         public DbSet<Teacher> Teachers => Set<Teacher>();
         public DbSet<Student> Students => Set<Student>();
+
+        // NOTE: There is a issue with the database when it is being created, so it cannot be created. Issue states that it comes from a connection-fault or faulty-instance. Will look into it.
     }
 }
